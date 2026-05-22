b91d7bfbdaefee21621ed75fa89e621671168ca5
AlokaAryayudha <Aaloka221@gmail.com>
2026-05-22 21:27:15 +0700

    Update userdata

--- Data/UserData.ts
+++ Data/UserData.ts
@@ -10,3 +10,34 @@ export const CheckoutData = {
     postalCode: '',
   },
 };
+
+export const UserData = {
+    validUser: {
+    username: 'standard_user',
+    password: 'secret_sauce',
+  },
+  lockedUser: {
+    username: 'locked_out_user',
+    password: 'secret_sauce',
+  },
+  problemUser: {
+    username: 'problem_user',
+    password: 'secret_sauce',
+  },
+  performanceUser: {
+    username: 'performance_glitch_user',
+    password: 'secret_sauce',
+  },
+  errorUser: {
+    username: 'error_user',
+    password: 'secret_sauce',
+  },
+  visualUser: {
+    username: 'visual_user',
+    password: 'secret_sauce',
+  },
+  invalidUser: {
+    username: 'wrong_user',
+    password: 'wrong_password',
+  },
+};
